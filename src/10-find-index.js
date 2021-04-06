e86da3bb80a1038ed3e2b1a271401c2d1287ff06
serrrgun <serrrgun@mail.ru>
2021-04-06 22:50:51 +0300

    feat: add find-index task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,28 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const lengthArr = array.length;
+
+  if (lengthArr > 100) {
+    let start = 0;
+    let end = array.length;
+    let pivot = Math.floor((start + end) / 2);
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[pivot] !== value) {
+        if (value < array[pivot]) end = pivot;
+        else start = pivot;
+        pivot = Math.floor((start + end) / 2);
+      }
+      if (array[pivot] === value) return i;
+    }
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === value) return i;
+    }
+  }
+  return 'Nothing Found';
 }
 
 module.exports = findIndex;
